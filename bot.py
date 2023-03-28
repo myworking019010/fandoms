5695176d85d7847674ca2a5fb8b9cbbbe63a5e94
Alvaos420 <126457972+Alvaos420@users.noreply.github.com>
2023-03-28 17:11:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,3 +79,53 @@ def create_fandom_type_keyboard():
 
 
 # Обработчик выбора типа фандома
+@bot.callback_query_handler(func=lambda call: call.data in ["movies", "anime", "games"])
+def fandom_type_handler(call):
+    # Получаем пользователя из БД
+    user = users_collection.find_one({"telegram_id": call.message.chat.id})
+    # Обновляем статус анкеты в БД
+    questionnaires_collection.update_one({"user_id": call.message.chat.id}, {"$set": {"fandom_type": call.data}})
+    # Отправляем второй вопрос - выбор конкретного фандома
+    bot.send_message(call.message.chat.id, "Выберите ваш любимый фандом:", reply_markup=create_fandoms_keyboard(call.data))
+
+
+#Создание инлайн-клавиатуры для выбора конкретного фандома
+def create_fandoms_keyboard(fandom_type):
+    fandoms = fandoms_collection.find({"type": fandom_type})
+    keyboard = types.InlineKeyboardMarkup()
+    for fandom in fandoms:
+        button = types.InlineKeyboardButton(text=fandom["fandom_name"], callback_data=f'questionnaire_choose_fandom_{fandom["_id"]}')
+        keyboard.add(button)
+    return keyboard
+
+
+#Обработчик выбора конкретного фандома
+@bot.callback_query_handler(func=lambda call: str(call.data).startswith('questionnaire_choose_fandom_'))
+def fandom_handler(call):
+    # Получаем пользователя из БД
+    user = users_collection.find_one({"telegram_id": call.message.chat.id})
+    # Получаем фандом из call.data
+    fandom_id = ObjectId(str(call.data.replace('questionnaire_choose_fandom_', ''))
+    fandom = fandoms_collection.find_one({"_id": fandom_id})
+    # Обновляем запись в БД - конкретный фандом
+    users_collection.update_one({"_id": ObjectId(user['_id'])}, {"$set": {"fandom": fandom['name']}})
+    # Обновляем статус анкеты в БД
+    questionnaires_collection.update_one({"user_id": call.message.chat.id}, {"$set": {"status": "completed"}})
+    # Создаем инлайн-кнопку "Подписаться на новости фандома"
+    markup = types.InlineKeyboardMarkup()
+    button = types.InlineKeyboardButton(text="✉️ Подписаться на новости фандома", callback_data=f"subscribe_{fandom_id}")
+    markup.add(button)
+    # Отправляем сообщение с приветственным текстом и инлайн-кнопкой
+    bot.send_message(call.message.chat.id, messages.QUESTIONNAIRE_COMPLETED_MESSAGE.format(fandom=fandom['fandom_name']), reply_markup=markup)
+
+
+#Обработчик подписки на новости фандома
+@bot.callback_query_handler(func=lambda call: call.data.startswith("subscribe_"))
+def subscribe_handler(call):
+    # Получаем пользователя из БД
+    user = users_collection.find_one({"telegram_id": call.message.chat.id})
+    # Получаем фандом
+    fandom_id = call.data.replace('subscribe_', '')
+    # Создаем запись о подписке в БД
+    subscriptions_collection.insert_one({"user_id": user['telegram_id'], f"fandom_id_{fandom_id}": 'ok'})
+    bot.send_message(call.message.chat.id, 'Вы успешно подписались на новости фандома!')
